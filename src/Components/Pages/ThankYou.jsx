04c33a468e95c6c3f808fc21b3b29f6edb87c171
deanunvan <deanunvant@mail.com>
2025-04-08 14:30:14 +0200

    faviconico commit change

--- src/Components/Pages/ThankYou.jsx
+++ src/Components/Pages/ThankYou.jsx
@@ -12,62 +12,35 @@ export const ThankYou = () => {
     const sr = ScrollReveal({
       distance: '60px',
       duration: 800,
-      delay: 200
-    });
-
-    // Logo animation
-    sr.reveal('.logo2', {
-      origin: 'top',
-      delay: 200
-    });
-
-    // Content animations
-    sr.reveal('.thank-you-content h1', {
-      origin: 'bottom',
-      delay: 600,
-      distance: '50px'
-    });
-
-    sr.reveal('.thank-you-content p', {
-      origin: 'bottom',
-      delay: 800,
-      distance: '30px'
-    });
-
-    sr.reveal('.home-button', {
-      origin: 'bottom',
-      delay: 900,
-      distance: '20px'
-    });
-
-    sr.reveal('.thank-you-image', {
-      origin: 'bottom',
       delay: 200,
-      distance: '20px'
     });
 
-    return () => sr.destroy();
-  }, []);
+    sr.reveal('.thank-you-content h1', { origin: 'bottom', delay: 600, distance: '50px' });
+    sr.reveal('.thank-you-content p', { origin: 'bottom', delay: 800, distance: '30px' });
+    sr.reveal('.thank-you-image', { origin: 'bottom', delay: 200, distance: '20px' });
 
-  const handleReload = () => {
-    navigate('/');
-    window.location.reload();
-  };
+    // Redirect to homepage after 10 seconds
+    const timer = setTimeout(() => {
+      navigate('/');
+    }, 5000);
+
+    return () => {
+      sr.destroy();
+      clearTimeout(timer);
+    };
+  }, [navigate]);
 
   return (
     <div>
       <div className="thank-you-page">
-      <img className="logo2" src={Logo2} alt="Groundcentered Logo" />
-      <div className="thank-you-content">
-        <h1>Thank You!</h1>
-        <img src={qn1} alt="Drilling Equipment" className="thank-you-image" />
-        <p>You have successfully joined our waiting list. We will keep you posted.</p>
-        <button onClick={handleReload} className="home-button">
-          Done!
-        </button>
+        <img className="logo2" src={Logo2} alt="Groundcentered Logo" />
+        <div className="thank-you-content">
+          <h1>Thank You!</h1>
+          <img src={qn1} alt="Drilling Equipment" className="thank-you-image" />
+          <p>You have successfully joined our waiting list. We will keep you posted.</p>
+        </div>
       </div>
-    </div>
-    <footer className="footer">
+      <footer className="footer">
         <p>groundcentered.com © 2025 All Rights Reserved</p>
       </footer>
     </div>
